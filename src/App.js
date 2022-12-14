967a4f01e993b26808dc61da1013aaca0b22ecba
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2022-12-14 17:47:02 -0300

    14.12.2022

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import { useState } from 'react';
 import parse from 'html-react-parser';
 import readTextFile from './scripts/readTextFile.js';
 import file from '../src/documents/teste.txt';
+import './App.css';
 
 function App() {
   const [text, setText] = useState('');
   readTextFile(file).then((res) => setText(res));
 
-  return <>{parse(text)}</>;
+  return (
+    <div className='wrapper'>
+      {parse(text)}
+    </div>
+  ) ;
 }
 
 export default App;
--- src/scripts/readTextFile.js
+++ src/scripts/readTextFile.js
@@ -2,13 +2,25 @@ async function readTextFile(path) {
   let response = await fetch(path)
     .then(res => res.text())
     .then(data => {
+      //replace image
+      let urls = data.split('[img](');
+      for (let i = 1; i < urls.length; i++) {
+        let url = urls[i].split(')')[0];
+        data = data.replaceAll(`[img](${url})\r\n`, `<div className="wrapper-picture"><img src='${url}' className="picture"></img></div>`);
+      }
+
       //replace title
       data = data.replaceAll('[titulo]', '<h2>');
       data = data.replaceAll('[/titulo]\r\n', '</h2>');
       
+      //replace wrapper-paragraph
+      data = data.replaceAll('[p]\r\n', '<div className="wrapper-paragraph">');
+      data = data.replaceAll('[p,left]\r\n', '<div className="wrapper-paragraph wrapper-paragraph-left">');
+      data = data.replaceAll('[/p]\r\n', '</div>');
+      
       //replace text
-      data = data.replaceAll('[texto]\r\n', '<p>');
-      data = data.replaceAll('[/texto]\r\n', '</p>');
+      data = data.replaceAll('[txt]\r\n', '<p className="wrapper-text">');
+      data = data.replaceAll('[/txt]\r\n', '</p>');
 
       //replace strong
       data = data.replaceAll('[b]', '<b>');
@@ -23,7 +35,6 @@ async function readTextFile(path) {
 
       return data;
     });
-    
   return response;
 }
 
