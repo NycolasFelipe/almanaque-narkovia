466af4df552f5fb329601460fae64d4bc398d03c
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2022-12-15 15:06:29 -0300

    Criação da homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,21 @@
+import './styles.css';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
 function Home() {
-  return <h1>Home</h1>
+  const navigate = useNavigate();
+
+  return (
+    <div className='wrapper-home'>
+      <div className='wrapper-home-blur'></div>
+      <div className='wrapper-home-title'>
+        <h1>Almanaque de Narkóvia</h1>
+      </div>
+      <div className='wrapper-home-options'>
+        <Button className='home-button' variant='outline' onClick={() => navigate('/guia-para-criacao-de-personagem')}>Guia para criação de personagem</Button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
