f66971f621821917f6a6af1ee161439e89fefdb7
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2022-12-16 21:26:52 -0300

    Aprimoramento da função de marcação do documento

--- src/scripts/readTextFile.js
+++ src/scripts/readTextFile.js
@@ -1,47 +1,127 @@
+/*
+  TAGS DE MARCAÇÃO
+  @titulo titulo @titulo            h2
+  @subtitulo subtitulo @subtitulo   h3
+  @p @p                             div
+  @pe @pe                           div (para que a imagem fique na esquerda do parágrafo)
+  @t @t                             p
+  @fill                             div (para completar parágrafos com caracteres insuficientes)
+  @img(url)                         img
+  
+  SIMBOLOS PARA ESTILIZAÇÃO
+  *texto*   negrito
+  $texto$   underline
+  _texto_   italico
+*/
+
+function replaceDoubleTag(text, char, newChar) {
+  text = text.split(char);
+  for (let i = 0; i < text.length-1; i++) {
+    if (i % 2 == 0) {
+      switch(newChar) {
+        case 'underline':
+          text[i] += '<span className="wrapper-paragraph-underline">';
+          break;
+        
+        case 'h2':
+          text[i] += '<h2 className="wrapper-paragraph-title">';
+          break;
+
+        case 'h3':
+          text[i] += '<h3 className="wrapper-paragraph-subtitle">';
+          break;
+          
+        case 'p':
+          text[i] += '<div className="wrapper-paragraph">';
+          break;
+
+        case 'pe':
+          text[i] += '<div className="wrapper-paragraph wrapper-paragraph-left">';
+          break;
+
+        case 't':
+          text[i] += '<p className="wrapper-paragraph-text">';
+          break;
+
+        case 'fill':
+          text[i] += '<div className="paragraph-text-fill">';
+          break;
+        
+        default:
+          text[i] += `<${newChar}>`;
+          break;
+      }
+    }
+    else {
+      switch(newChar) {
+        case 'underline': 
+          text[i] += '</span>';
+          break;
+
+        case 'p':
+          text[i] += '</div>';
+          break;
+
+        case 'pe':
+          text[i] += '</div>';
+          break;
+          
+        case 't':
+          text[i] += '</p>';
+          break;
+        
+        case 'fill':
+          text[i] += '</div>';
+          break;
+        
+        default:
+          text[i] += `</${newChar}>`;
+          break;
+      }
+    }
+  }
+  text = text.join('');
+  return text;
+}
+
 async function readTextFile(path) {
   let response = await fetch(path)
     .then(res => res.text())
     .then(data => {
       //replace image
-      let urls = data.split('[img](');
+      let urls = data.split('@img(');
       for (let i = 1; i < urls.length; i++) {
         let url = urls[i].split(')')[0];
-        data = data.replaceAll(`[img](${url})`, `<div className="wrapper-picture"><img src='${url}' className="picture"></img></div>`);
+        data = data.replaceAll(
+          `@img(${url})`, 
+          `<div className="wrapper-picture"><img src='${url}' className="picture"></img></div>`
+        );
       }
 
       //replace title
-      data = data.replaceAll('[titulo]', '<h2 className="wrapper-paragraph-title">');
-      data = data.replaceAll('[/titulo]', '</h2>');
+      data = replaceDoubleTag(data, '@titulo', 'h2');
 
       //replace subtitle
-      data = data.replaceAll('[subtitulo]', '<h3 className="wrapper-paragraph-subtitle">');
-      data = data.replaceAll('[/subtitulo]', '</h3>');
+      data = replaceDoubleTag(data, '@subtitulo', 'h3');
       
       //replace wrapper-paragraph
-      data = data.replaceAll('[p]', '<div className="wrapper-paragraph">');
-      data = data.replaceAll('[p,left]', '<div className="wrapper-paragraph wrapper-paragraph-left">');
-      data = data.replaceAll('[/p]', '</div>');
+      data = replaceDoubleTag(data, '@pe', 'pe');
+      data = replaceDoubleTag(data, '@p', 'p');
       
       //replace text
-      data = data.replaceAll('[txt]', '<p className="wrapper-paragraph-text">');
-      data = data.replaceAll('[/txt]', '</p>');
-
+      data = replaceDoubleTag(data, '@t', 't');
+      
       //replace text fill
-      data = data.replaceAll('[txtfill]', '<div className="paragraph-text-fill">');
+      data = replaceDoubleTag(data, '@fill', 'fill');
 
       //replace bold
-
-      //replace bold
-      data = data.replaceAll('[b]', '<b>');
-      data = data.replaceAll('[/b]', '</b>');
+      data = replaceDoubleTag(data, '*', 'b');
 
       //replace underline
-      data = data.replaceAll('[u]', '<span className="wrapper-paragraph-underline">');
-      data = data.replaceAll('[/u]', '</span>');
+      data = replaceDoubleTag(data, '$', 'underline');
 
       //replace italic
-      data = data.replaceAll('[i]', '<i>');
-      data = data.replaceAll('[/i]', '</i>');
+      data = replaceDoubleTag(data, '_', 'i');
 
       return data;
     });
